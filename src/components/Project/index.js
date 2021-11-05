9f3b13f94a01afd58ffa650a07fce7a72abb0a1a
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-11-05 09:27:24 -0400

    added 5 projects to the portfolio

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -11,11 +11,12 @@ function Project({ project }) {
     const githubPath = "https://github.com/bjackels5/";
 
     return (
-        <div className="col-md-3 col-sm-12 mb-1 project">
-           <img src={require(`../../assets/images/${project.image}`).default} alt={project.name}></img>
+        <div className="col-md-4 col-sm-12 mb-4 project">
+            <img className="proj-img" src={require(`../../assets/images/${project.image}`).default} alt={project.name}></img>
             <p className="card-text">{project.description}</p>
+            {/* to change the color of the icon, add color="thecolor" to the icon. <FaGithub color="magenta" />, e.g. */}
             <a className="proj-link" href={`${githubPath}${project.githubProject}`} target="_blank" rel="noreferrer"><FaGithub /></a>
-            <a className="proj-link" href={`${project.appLink}`} target="_blank" rel="noreferrer"><FaPlayCircle /></a>
+            <a className="proj-link float-right" href={`${project.appLink}`} target="_blank" rel="noreferrer"><FaPlayCircle /></a>
         </div>
     )
 }
