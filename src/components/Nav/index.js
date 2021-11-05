570b769f09b6cd5d4417d4cc761693cac8f06f13
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-11-05 16:40:34 -0400

    menu was disappearing when resize to mobile device. fixed it

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -12,12 +12,8 @@ function Nav(props) {
     } = props;
 
     return (
-        <nav className="navbar navbar-expand-lg ml-4">
+        <nav className="navbar navbar-expand ml-4">
             <a className="navbar-brand corben mt-0 pt-0" href="/">Brenda Jackels</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse">
                 <div className="navbar-nav">
                     {menuSelections.map((menuSelection) => (
                         <li className={`nav-item nav-link ${currentSelection.name === menuSelection.name && 'navActive'}`} key={menuSelection.name}>
@@ -30,7 +26,6 @@ function Nav(props) {
                         </li>
                     ))}
                 </div>
-            </div>
         </nav>
     )
 }
