c240185dd2854b5fd5bd9d7f626ab85f5cf0acf2
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-11-05 15:30:57 -0400

    style the contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,18 +2,18 @@
 ContactForm Component
 */
 
-import React, { useState}  from 'react';
+import React, { useState } from 'react';
 
 function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
- 
+
     return re.test(String(email).toLowerCase());
-  }
+}
 
 function ContactForm() {
 
-    const [ formState, setFormState] = useState({ name: '', email: '', message: ''});
-    const [ errorMessage, setErrorMessage] = useState('');
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
 
     const { name, email, message } = formState;
 
@@ -24,10 +24,10 @@ function ContactForm() {
             setErrorMessage(isValid ? '' : 'Your email is invalid');
         }
         else {
-            setErrorMessage( e.target.value.length ? '' : `${e.target.name} is required.`);
+            setErrorMessage(e.target.value.length ? '' : `${e.target.name} is required.`);
         }
         if (!errorMessage) {
-            setFormState({...formState, [ e.target.name]: e.target.value});
+            setFormState({ ...formState, [e.target.name]: e.target.value });
         }
         console.log('errorMessage:', errorMessage);
     }
@@ -40,27 +40,39 @@ function ContactForm() {
     }
 
     return (
-        <section>
-            <h1> Contact me</h1>
+        <section className="container py-4">
+
+            {/* <!-- Bootstrap 5 starter form --> */}
             <form id="contact-form" onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" defaultValue = { name } onBlur={handleChange} />
+
+                {/* <!-- Name input --> */}
+                <div className="mb-3">
+                    <label className="form-label" htmlFor="name">Name</label>
+                    <input className="form-control" name="name" id="name" type="text" defaultValue={name} placeholder={name} onBlur={handleChange} />
                 </div>
-                <div>
-                    <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" defaultValue = { email } onBlur={handleChange}/>
+
+                {/* <!-- Email address input --> */}
+                <div className="mb-3">
+                    <label className="form-label" htmlFor="email">Email Address</label>
+                    <input className="form-control" id="email" name="email" type="email" defaultValue={email} placeholder={email} onBlur={handleChange} />
                 </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" defaultValue = { message } onBlur={handleChange}/>
+
+                {/* <!-- Message input --> */}
+                <div className="mb-3">
+                    <label className="form-label" htmlFor="message">Message</label>
+                    <textarea className="form-control" id="message" name="message" type="text" defaultValue={message} placeholder={message} onBlur={handleChange} />
                 </div>
-                { errorMessage && (
+
+                {errorMessage && (
                     <div>
                         <p className="error-text">{errorMessage}</p>
                     </div>
                 )}
-                <button type="submit">Submit</button>
+
+                {/* <!-- Form submit button --> */}
+                <div class="d-grid">
+                    <button class="btn btn-lg" type="submit">Submit</button>
+                </div>
             </form>
         </section>
     )
