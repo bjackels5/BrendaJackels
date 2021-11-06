3e7f7e3e2299975ffff0a38d9e40188987cdbbd1
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-11-06 17:40:04 -0400

    fixed the navbar so it'll wrap on small devices

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -12,9 +12,9 @@ function Nav(props) {
     } = props;
 
     return (
-        <nav className="navbar navbar-expand ml-4 bg-custom-nav fixed-top">
+        <nav className="navbar nav navbar-expand ml-4 bg-custom-nav fixed-top">
             <a className="navbar-brand corben mt-0 pt-0" href="/bjackels5-portfolio">Brenda Jackels</a>
-                <div className="navbar-nav">
+                <div className="navbar-nav nav">
                     {menuSelections.map((menuSelection) => (
                         <li className={`nav-item nav-link ${currentSelection.name === menuSelection.name && 'navActive'}`} key={menuSelection.name}>
                             <span onClick={() => {
